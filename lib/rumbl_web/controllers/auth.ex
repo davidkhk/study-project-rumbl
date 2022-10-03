2b3330f9820161e03ebb6a48c826ad0c2e91bbd6
David K <davidkhk@gmail.com>
2022-10-03 16:10:13 -0300

    feat: generate video resource

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,4 +1,6 @@
 defmodule RumblWeb.Auth do
+  import Phoenix.Controller
+  alias RumblWeb.Router.Helpers, as: Routes
   import Plug.Conn
 
   def init(opts), do: opts
@@ -18,5 +20,16 @@ defmodule RumblWeb.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
